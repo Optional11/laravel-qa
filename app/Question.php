a4ce2665c726fd736d75fb1a22733fceab98aeca
Optional11 <you@example.comsredl.jakub@seznam.cz>
2020-03-09 19:17:07 +0100

    Generate Question model and set relation with User

---
+++ app/Question.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    // set massassigment fields
+    protected $fillable = ['title', 'body'];
+
+    // defining relationship among user and Question
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    //slug / we do not want set manually, so we need to set umutator
+    //fun has to start with set
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+}
