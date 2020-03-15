7c0e981706f861a3670e1350c305e586924365d5
Optional11 <you@example.comsredl.jakub@seznam.cz>
2020-03-15 02:25:33 +0100

    U best_answer_id on Q tbl when A removed in db

--- app/Answer.php
+++ app/Answer.php
@@ -35,13 +35,21 @@ class Answer extends Model
         return $this->created_at->diffForHumans();
     }
 
+    public function getStatusAttribute()
+    {
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
+    }
+
     public static function boot()
     {
         parent::boot();
 
         static::created(function ($answer) {
-            $answer->question->increment('answers_count');
-            $answer->question->save();            
-        });        
+            $answer->question->increment('answers_count');            
+        });
+        
+        static::deleted(function ($answer) {
+            $answer->question->decrement('answers_count');
+        });
     }
 }
