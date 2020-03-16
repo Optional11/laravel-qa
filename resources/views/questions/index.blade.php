423ede3f1ebc0c70f898fef21fcada1fc555b0e7
Optional11 <you@example.comsredl.jakub@seznam.cz>
2020-03-16 17:39:14 +0100

    implement votes for Q and A

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -9,35 +9,34 @@
                     <div class="d-flex align-items-center">
                         <h2>All Questions</h2>
                         <div class="ml-auto">
-                            <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary" >Ask Question</a>
+                            <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Ask Question</a>
                         </div>
                     </div>
                     
                 </div>
 
                 <div class="card-body">
-                    @include('layouts._messages')
+                    @include ('layouts._messages')
+
                     @foreach ($questions as $question)
                         <div class="media">
                             <div class="d-flex flex-column counters">
                                 <div class="vote">
-                                    <strong>{{ $question->votes }}</strong> {{ Str::plural('vote', $question->votes) }}
-                                </div>
+                                    <strong>{{ $question->votes_count }}</strong> {{ Str::plural('vote', $question->votes_count) }}
+                                </div>                            
                                 <div class="status {{ $question->status }}">
                                     <strong>{{ $question->answers_count }}</strong> {{ Str::plural('answer', $question->answers_count) }}
-                                </div>
+                                </div>                            
                                 <div class="view">
                                     {{ $question->views . " " . Str::plural('view', $question->views) }}
-                                </div>
+                                </div>                            
                             </div>
                             <div class="media-body">
                                 <div class="d-flex align-items-center">
-                                    <h3 class="mt-0"><a href="{{ $question->url}}">{{ $question->title }}</a></h3>
+                                    <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                     <div class="ml-auto">
                                         @can ('update', $question)
-                                            <a href="{{ route('questions.edit' , $question->id) }}" class="btn btn-sm btn-outline-info">
-                                                Edit
-                                            </a>
+                                            <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
                                         @endcan
                                         @can ('delete', $question)
                                             <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
@@ -48,21 +47,20 @@
                                         @endcan
                                     </div>
                                 </div>
-                                
                                 <p class="lead">
-                                Asked by
-                                <a href="{{ $question->user->url}}">{{ $question->user->name }}</a>
-                                <small class="text-muted">{{ $question->created_date }}</small>
+                                    Asked by 
+                                    <a href="{{ $question->user->url }}">{{ $question->user->name }}</a> 
+                                    <small class="text-muted">{{ $question->created_date }}</small>
                                 </p>
                                 {{ Str::limit($question->body, 250) }}
-                            </div>
+                            </div>                        
                         </div>
                         <hr>
                     @endforeach
+
                     <div class="mx-auto">
                         {{ $questions->links() }}
                     </div>
-                    
                 </div>
             </div>
         </div>
