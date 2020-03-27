7ab5c6fa318ed8c8101376b27cdc4a7bcef7b86a
Optional11 <you@example.comsredl.jakub@seznam.cz>
2020-03-27 22:56:08 +0100

    Create A component & implement vue list rendering

--- resources/js/app.js
+++ resources/js/app.js
@@ -30,8 +30,8 @@ Vue.use(Authorization);
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('user-info', require('./components/UserInfo.vue').default);
-Vue.component('answer', require('./components/Answer.vue').default);
 Vue.component('vote', require('./components/Vote.vue').default);
+Vue.component('answers', require('./components/Answers.vue').default);
 
 
 
